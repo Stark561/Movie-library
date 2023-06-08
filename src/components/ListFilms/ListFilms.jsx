df593fce6489875359d6cbb453a82aafd53e95ee
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-06-08 17:38:36 +0300

    fix home

--- src/components/ListFilms/ListFilms.jsx
+++ src/components/ListFilms/ListFilms.jsx
@@ -5,17 +5,34 @@ import PropTypes from 'prop-types';
 export function ListFilms({ films }) {
   const location = useLocation();
 
+  const standartImage =
+  'http://dummyimage.com/150x60/99cccc.gif&text=The+image!';
   return (
     <List>
-      {films.map(({ title, id, name }) => {
+      {films.map(({ title, id, name, poster_path, backdrop_path }) => {
         return (
           <li key={id}>
             <Link
               to={location.pathname === '/' ? `movies/${id}` : `${id}`}
               state={{ from: location }}
-            >
-              {title ?? name}
-            </Link>
+              >
+            <div>
+              <img
+              style={{ width: 200 }}
+              src={
+                poster_path || backdrop_path
+                ? `https://image.tmdb.org/t/p/w200${
+                  poster_path || backdrop_path
+                }`
+                : standartImage
+              }
+              alt={title ?? name}
+              />
+              </div>
+              <div>
+            {title ?? name}
+              </div>
+              </Link>
           </li>
         );
       })}
--- src/components/ListFilms/ListFilms.styled.jsx
+++ src/components/ListFilms/ListFilms.styled.jsx
@@ -3,6 +3,13 @@ import styled from '@emotion/styled';
 export const List = styled.ul`
   list-style: none;
   padding: 0;
+  display: grid;
+  grid-gap: 16px;
+    grid-template-columns: repeat(auto-fill,minmax(320px,1fr));
+    list-style: none;
+    margin: 0 auto;
+    max-width: calc(100vw - 48px);
+    padding: 0;
 
   a {
     text-decoration: none;
@@ -10,6 +17,7 @@ export const List = styled.ul`
     font-size: 18px;
     line-height: 1.5;
     font-weight: 500;
+    max-width: 200px;
 
     &:hover {
       color: #0b7ee4;
